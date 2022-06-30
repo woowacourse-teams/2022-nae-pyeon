19568d2f3186152f7050194ffd0a09808f9ab362
soyi47 <soyi.dev@gmail.com>
2022-06-30 20:02:31 +0900

    chore: 리액트, 타입스크립트 환경설정
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>

---
+++ frontend/webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const mode = process.env.NODE_ENV || "development";
+
+module.exports = {
+  mode,
+  devServer: {
+    historyApiFallback: true,
+    port: 3000,
+    hot: true,
+  },
+  entry: {
+    app: path.join(__dirname, "src/index.tsx"),
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["babel-loader", "ts-loader"],
+      },
+    ],
+  },
+
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+};
