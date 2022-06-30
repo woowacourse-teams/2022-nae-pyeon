a5482c0878b9c840a3b49bf0ae78342aa0e7e2ac
soyi47 <soyi.dev@gmail.com>
2022-06-30 21:32:46 +0900

    chore: emotion 설치
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import styled from "@emotion/styled";
+
+const Style = styled.div`
+  color: hotpink;
+`;
 
 const App = () => {
-  return <div>APP!!</div>;
+  return <Style>APP!!</Style>;
 };
 
 export default App;
