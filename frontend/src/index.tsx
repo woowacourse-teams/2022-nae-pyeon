1b75205127d02d415f3ce48cbbb602b9a247d1e5
soyi47 <soyi.dev@gmail.com>
2022-07-01 13:32:44 +0900

    chore: eslint, prettier 설치 및 설정
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 
 if (process.env.NODE_ENV === "development") {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
   const { worker } = require("./mocks/browser");
   worker.start();
 }
