52278e33dbaa84f419dae7fce711fb1fd90bfc34
asebn1 <asebn121@gmail.com>
2022-07-02 19:58:17 +0900

    feat: Member Repository 기능 및 테스트 구현

--- backend/src/main/java/com/woowacourse/naepyeon/repository/JpaMemberRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/JpaMemberRepository.java
@@ -1,5 +1,27 @@
 package com.woowacourse.naepyeon.repository;
 
+import com.woowacourse.naepyeon.domain.Member;
+import com.woowacourse.naepyeon.repository.jpa.MemberJpaDao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
 public class JpaMemberRepository implements MemberRepository {
 
+    private final MemberJpaDao memberJpaDao;
+
+    public Long save(final Member member) {
+        return memberJpaDao.save(member).getId();
+    }
+
+    public Optional<Member> findById(final Long memberId) {
+        return memberJpaDao.findById(memberId);
+    }
+
+    public void delete(final Long memberId) {
+        memberJpaDao.deleteById(memberId);
+    }
 }
