4f4b9f084ec753242ceac85a8a6726e02762d707
Soyi Jeon <soyi.dev@gmail.com>
2022-07-02 11:32:03 +0900

    chore: 프론트엔드 환경설정 (#15)
    * chore: 리액트, 타입스크립트 환경설정
    
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>
    
    * chore: storybook 설치
    
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>
    
    * chore: msw 설치
    
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>
    
    * chore: emotion 설치
    
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>
    
    * chore: eslint, prettier 설치 및 설정
    
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>
    
    Co-authored-by: SunHo Park <prefer2@users.noreply.github.com>

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Style = styled.div`
+  color: hotpink;
+`;
+
+const App = () => {
+  return <Style>APP!!</Style>;
+};
+
+export default App;
---
+++ frontend/src/mocks/handlers.js
@@ -0,0 +1,8 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+export const handlers = [
+  // Handles a POST /login request
+  rest.post("/login", null),
+  // Handles a GET /user request
+  rest.get("/user", null),
+];
