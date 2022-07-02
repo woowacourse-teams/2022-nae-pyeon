a0670412bc7229cafd15b57954a544fd52ee207e
asebn1 <asebn121@gmail.com>
2022-07-02 23:05:08 +0900

    feat: Team Repository 기능 및 테스트 구현

--- backend/src/main/java/com/woowacourse/naepyeon/repository/TeamRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/TeamRepository.java
@@ -1,4 +1,14 @@
 package com.woowacourse.naepyeon.repository;
 
+import com.woowacourse.naepyeon.domain.Team;
+
+import java.util.Optional;
+
 public interface TeamRepository {
+
+    Long save(final Team team);
+
+    Optional<Team> findById(final Long teamId);
+
+    void delete(final Long teamId);
 }
