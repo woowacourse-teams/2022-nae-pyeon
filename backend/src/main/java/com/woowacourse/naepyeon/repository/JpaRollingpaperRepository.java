475fd64cc005a9088828b97acf7e2bed40081d36
kth990303 <kth990303@naver.com>
2022-07-03 13:09:15 +0900

    feat: Rollingpaper Repository 기능 및 테스트 구현
    Co-authored-by: yxxnghwan <younghwan960@gmail.com>

--- backend/src/main/java/com/woowacourse/naepyeon/repository/JpaRollingpaperRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/JpaRollingpaperRepository.java
@@ -1,4 +1,37 @@
 package com.woowacourse.naepyeon.repository;
 
+import com.woowacourse.naepyeon.domain.Rollingpaper;
+import com.woowacourse.naepyeon.repository.jpa.RollingpaperJpaDao;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
 public class JpaRollingpaperRepository implements RollingpaperRepository {
+
+    private final RollingpaperJpaDao rollingpaperJpaDao;
+
+    @Override
+    public Long save(final Rollingpaper rollingpaper) {
+        return rollingpaperJpaDao.save(rollingpaper)
+                .getId();
+    }
+
+    @Override
+    public Optional<Rollingpaper> findById(final Long id) {
+        return rollingpaperJpaDao.findById(id);
+    }
+
+    @Override
+    public void update(final Long id, final String newTitle) {
+        final Rollingpaper rollingpaper = rollingpaperJpaDao.findById(id)
+                .orElseThrow(IllegalArgumentException::new);
+        rollingpaper.changeTitle(newTitle);
+    }
+
+    @Override
+    public void delete(final Long id) {
+        rollingpaperJpaDao.deleteById(id);
+    }
 }
--- backend/src/main/java/com/woowacourse/naepyeon/repository/RollingpaperRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/RollingpaperRepository.java
@@ -1,4 +1,15 @@
 package com.woowacourse.naepyeon.repository;
 
+import com.woowacourse.naepyeon.domain.Rollingpaper;
+import java.util.Optional;
+
 public interface RollingpaperRepository {
+
+    Long save(final Rollingpaper rollingpaper);
+
+    Optional<Rollingpaper> findById(final Long id);
+
+    void update(final Long id, final String newTitle);
+
+    void delete(final Long id);
 }
