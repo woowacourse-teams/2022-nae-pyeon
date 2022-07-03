6d99c059325e397b39a461796df389b60ee514f8
prefer2 <sunho6200@gmail.com>
2022-07-03 17:03:43 +0900

    feat: LetterPaper 컴포넌트 구현
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

---
+++ frontend/src/components/LetterPaper.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+import IconButton from "@components/IconButton";
+import RollingpaperMessage from "@components/RollingpaperMessage";
+
+import { BiPencil } from "react-icons/bi";
+
+interface Message {
+  content: string;
+  from: string;
+  authorId: number;
+}
+
+interface LetterPaperProp {
+  to: string;
+  messageList: Message[];
+}
+
+const StyledLetterPaper = styled.div`
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+
+  background: #f7f7f7;
+  border-radius: 8px;
+`;
+
+const StyledLetterPaperTop = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  height: 20px;
+
+  margin-bottom: 20px;
+`;
+
+const StyledTo = styled.h3`
+  font-weight: 600;
+`;
+
+const StyledMessageList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-row-gap: 20px;
+  grid-column-gap: 20px;
+  justify-items: center;
+
+  height: calc(100% - 40px);
+  overflow-y: scroll;
+
+  @media only screen and (min-width: 960px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media only screen and (min-width: 1280px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+`;
+
+const LetterPaper = ({ to, messageList }: LetterPaperProp) => {
+  return (
+    <StyledLetterPaper>
+      <StyledLetterPaperTop>
+        <StyledTo>To. {to}</StyledTo>
+        <IconButton size="small">
+          <BiPencil />
+        </IconButton>
+      </StyledLetterPaperTop>
+      <StyledMessageList>
+        {messageList.map((message) => (
+          <RollingpaperMessage
+            content={message.content}
+            author={message.from}
+          />
+        ))}
+      </StyledMessageList>
+    </StyledLetterPaper>
+  );
+};
+
+export default LetterPaper;
