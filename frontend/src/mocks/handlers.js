53babd53fcd9a82895eb9b131e8fcb51c133241e
prefer2 <sunho6200@gmail.com>
2022-07-04 20:46:20 +0900

    test: 롤링페이퍼 관련 API mocking
    - 롤링페이퍼 단건 조회
    - 메시지 생성
    - 개인 롤링페이퍼 생성
    
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,8 +1,43 @@
-// src/mocks/handlers.js
 import { rest } from "msw";
+import dummy from "./dummy/rollingpapers.json";
+
+const rollingpapersDummy = dummy.rollingpapers;
+
 export const handlers = [
-  // Handles a POST /login request
-  rest.post("/login", null),
-  // Handles a GET /user request
-  rest.get("/user", null),
+  // 롤링페이퍼 단건 조회
+  rest.get(
+    "/api/v1/teams/:teamId/rollingpapers/:rollingpaperId",
+    (req, res, ctx) => {
+      const { teamId, rollingpaperId } = req.params;
+
+      const result = rollingpapersDummy.find(
+        (rollingpaper) => rollingpaper.id === rollingpaperId
+      );
+
+      return res(ctx.json(result));
+    }
+  ),
+
+  // 메시지 생성
+  rest.post(
+    "/api/v1/rollingpapers/:rollingpaperId/messages",
+    (req, res, ctx) => {
+      const { rollingpaperId } = req.params;
+      const { content, authorId } = JSON.parse(req.body);
+
+      const result = rollingpapersDummy.find(
+        (rollingpaper) => rollingpaper.id === rollingpaperId
+      );
+
+      return res(ctx.status(201));
+    }
+  ),
+
+  // 개인 롤링페이퍼 생성
+  rest.post("/api/v1/teams/:teamId/rollingpapers", (req, res, ctx) => {
+    const { teamId } = req.params;
+    const { title, memberId } = JSON.parse(req.body);
+
+    return res(ctx.status(201));
+  }),
 ];
