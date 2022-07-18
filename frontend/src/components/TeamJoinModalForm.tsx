7a1fbc9ac232c6ae58f02283d5913ee4f7206021
soyi47 <soyi.dev@gmail.com>
2022-07-18 13:57:28 +0900

    feat: 모임 가입 요청 로직 작성

--- frontend/src/components/TeamJoinModalForm.tsx
+++ frontend/src/components/TeamJoinModalForm.tsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { useMutation } from "react-query";
 import styled from "@emotion/styled";
+
+import appClient from "@/api";
 
 import LineButton from "@/components/LineButton";
 import Modal from "@/components/Modal";
@@ -11,20 +15,51 @@ interface TeamJoinModalFormProp {
   onClickCloseButton: React.MouseEventHandler<HTMLButtonElement>;
 }
 
+interface TeamJoinFormInfo {
+  nickname: string;
+}
+
 const TeamJoinModalForm = ({ onClickCloseButton }: TeamJoinModalFormProp) => {
-  const [username, setUsername] = useState("");
+  const navigate = useNavigate();
+  const { teamId } = useParams();
+  const [nickname, setNickname] = useState("");
+
+  const { mutate: joinTeam } = useMutation(
+    async ({ nickname }: TeamJoinFormInfo) => {
+      const response = await appClient.post(`/teams/${teamId}`, { nickname });
+      return response.data;
+    },
+    {
+      onSuccess: () => {
+        alert("가입 성공!");
+        navigate(`/teams/${teamId}`);
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    }
+  );
+
+  const handleTeamJoinSubmit: React.FormEventHandler<HTMLFormElement> = () => {
+    if (!REGEX.USERNAME.test(nickname)) {
+      alert("닉네임은 한글, 영어, 숫자만 가능합니다.");
+      return;
+    }
+
+    joinTeam({ nickname });
+  };
 
   return (
     <Modal onClickCloseButton={onClickCloseButton}>
-      <StyledJoinForm>
+      <StyledJoinForm onSubmit={handleTeamJoinSubmit}>
         <p>모임에서 사용할 닉네임을 입력해주세요. (2 ~ 20자)</p>
         <UnderlineInput
-          value={username}
-          setValue={setUsername}
+          value={nickname}
+          setValue={setNickname}
           pattern={REGEX.USERNAME.source}
           errorMessage="한글, 영어, 숫자 / 2 ~ 20자"
         />
-        <LineButton>모임 가입하기</LineButton>
+        <LineButton type="submit">모임 가입하기</LineButton>
       </StyledJoinForm>
     </Modal>
   );
