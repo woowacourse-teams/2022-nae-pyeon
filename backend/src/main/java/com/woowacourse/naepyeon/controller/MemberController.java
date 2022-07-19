a2af2cd95ad2075845b25415e6bc7ff45912236c
kth990303 <kth990303@naver.com>
2022-07-19 11:56:21 +0900

    style: import문 순서 변경 및 지나친 라인길이 변경

--- backend/src/main/java/com/woowacourse/naepyeon/controller/MemberController.java
+++ backend/src/main/java/com/woowacourse/naepyeon/controller/MemberController.java
@@ -6,6 +6,8 @@ import com.woowacourse.naepyeon.controller.dto.MemberRegisterRequest;
 import com.woowacourse.naepyeon.controller.dto.MemberUpdateRequest;
 import com.woowacourse.naepyeon.service.MemberService;
 import com.woowacourse.naepyeon.service.dto.MemberResponseDto;
+import java.net.URI;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,9 +17,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.validation.Valid;
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
