a02b0bb64c4972a615c04e95436dfa5169d22123
prefer2 <sunho6200@gmail.com>
2022-07-20 11:07:15 +0900

    feat: 모임명 정규표현식 추가
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -2,6 +2,7 @@ const REGEX = {
   EMAIL: /^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/,
   USERNAME: /^[가-힣a-zA-Z0-9]{2,20}$/,
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,20}$/,
+  TEAM_NAME: /^[가-힣a-zA-Z0-9]{1,20}$/,
 };
 
 const API_URL =
