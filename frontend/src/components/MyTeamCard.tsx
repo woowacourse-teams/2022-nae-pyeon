6ce59bd0b309941520c732389151d808200a13b0
prefer2 <sunho6200@gmail.com>
2022-07-21 01:19:12 +0900

    feat: myTeamsCard style 변경

--- frontend/src/components/MyTeamCard.tsx
+++ frontend/src/components/MyTeamCard.tsx
@@ -36,8 +36,8 @@ const StyledMyTeamCard = styled.div<StyledMyTeamCardProps>`
   display: flex;
   flex-direction: column;
 
-  width: 150px;
-  height: 190px;
+  width: 160px;
+  height: 210px;
 
   padding: 20px 14px;
   border-radius: 8px;
@@ -51,6 +51,20 @@ const StyledMyTeamCard = styled.div<StyledMyTeamCardProps>`
   &:hover {
     transform: scale(1.02);
     transition: transform 0.3s ease;
+  }
+
+  @media only screen and (min-width: 600px) {
+    width: 200px;
+    height: 250px;
+
+    padding: 24px 16px;
+  }
+
+  @media only screen and (min-width: 960px) {
+    width: 220px;
+    height: 290px;
+
+    padding: 24px 20px;
   }
 `;
 
@@ -73,12 +87,35 @@ const StyledName = styled.h2`
 
   font-size: 24px;
   font-weight: 600;
+
+  overflow: hidden;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+
+  @media only screen and (min-width: 960px) {
+    margin: 28px 0 8px 0;
+    -webkit-line-clamp: 2;
+  }
 `;
 
 const StyledDescription = styled.div`
   margin-top: 4px;
 
-  font-size: 14px;
+  font-size: 12px;
+  color: ${({ theme }) => theme.colors.GRAY_700};
+
+  overflow: hidden;
+
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 3;
+
+  @media only screen and (min-width: 600px) {
+    font-size: 14px;
+    -webkit-line-clamp: 4;
+  }
 `;
 
 export default MyTeamCard;
