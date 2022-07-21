99377a0506fad0cb5d1f2aaf691f4ac43df6501d
SunHo Park <67692759+prefer2@users.noreply.github.com>
2022-07-21 15:05:44 +0900

    feat: production 모드 설정 (#164)
    * feat: mode에 따른 webpack 설정 추가
    
    * feat: favicon 설정

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -1,18 +1,32 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
 const webpack = require("webpack");
-const mode = process.env.NODE_ENV || "development";
 
 module.exports = {
-  mode,
-  devServer: {
-    historyApiFallback: true,
-    port: 3000,
-    hot: true,
-  },
   entry: {
     app: path.join(__dirname, "src/index.tsx"),
   },
+  plugins: [
+    new webpack.ProvidePlugin({
+      React: "react",
+    }),
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+    new FaviconsWebpackPlugin({
+      logo: "./src/assets/images/logo_background.png",
+      favicons: {
+        appName: "내 편",
+        appShortName: "내 편",
+        appDescription: "내 마음을 편지로, 내 편",
+        theme_color: "#98DAFF",
+        background: "#fff",
+        display: "minimal-ui",
+      },
+    }),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx"],
     alias: {
@@ -20,7 +34,6 @@ module.exports = {
       "@components": path.resolve(__dirname, "src/components"),
     },
   },
-
   module: {
     rules: [
       {
@@ -33,20 +46,10 @@ module.exports = {
       },
     ],
   },
-
   output: {
     path: path.join(__dirname, "/dist"),
     publicPath: "/",
     filename: "bundle.js",
+    clean: true,
   },
-
-  plugins: [
-    new webpack.ProvidePlugin({
-      React: "react",
-    }),
-    new HtmlWebpackPlugin({
-      template: "./public/index.html",
-    }),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
 };
