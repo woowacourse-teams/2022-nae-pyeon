f8b40dcd2c4178ab36cbb5b116a10490eaffc337
soyi47 <soyi.dev@gmail.com>
2022-07-26 21:26:48 +0900

    feat: LetterPaper 내 MessageList 정렬 방식 변경

--- frontend/src/components/LetterPaper.tsx
+++ frontend/src/components/LetterPaper.tsx
@@ -1,12 +1,13 @@
-import React from "react";
-import styled from "@emotion/styled";
+import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import styled from "@emotion/styled";
 
 import IconButton from "@components/IconButton";
 import RollingpaperMessage from "@components/RollingpaperMessage";
 import { Message } from "@/types";
 
 import PencilIcon from "@/assets/icons/bx-pencil.svg";
+import { divideArrayByIndexRemainder } from "@/util";
 
 interface LetterPaperProp {
   to: string;
@@ -15,6 +16,12 @@ interface LetterPaperProp {
 
 const LetterPaper = ({ to, messageList }: LetterPaperProp) => {
   const navigate = useNavigate();
+  const [slicedMessageLists, setSlicedMessageLists] = useState<Message[][]>([
+    [],
+    [],
+    [],
+    [],
+  ]);
 
   const handleMessageWriteButtonClick: React.MouseEventHandler<
     HTMLButtonElement
@@ -22,6 +29,34 @@ const LetterPaper = ({ to, messageList }: LetterPaperProp) => {
     e.preventDefault();
     navigate(`message/new`);
   };
+
+  const updateSlicedMessageListByWindowWidth = () => {
+    const width = window.innerWidth;
+
+    let newSlicedMessageList;
+    if (width < 960) {
+      newSlicedMessageList = divideArrayByIndexRemainder(messageList, 2);
+    } else if (width < 1280) {
+      newSlicedMessageList = divideArrayByIndexRemainder(messageList, 3);
+    } else {
+      newSlicedMessageList = divideArrayByIndexRemainder(messageList, 4);
+    }
+
+    setSlicedMessageLists(newSlicedMessageList);
+  };
+
+  useEffect(() => {
+    updateSlicedMessageListByWindowWidth();
+  }, [messageList]);
+
+  useEffect(() => {
+    window.addEventListener("resize", updateSlicedMessageListByWindowWidth);
+    return () =>
+      window.removeEventListener(
+        "resize",
+        updateSlicedMessageListByWindowWidth
+      );
+  }, []);
 
   return (
     <StyledLetterPaper>
@@ -31,16 +66,20 @@ const LetterPaper = ({ to, messageList }: LetterPaperProp) => {
           <PencilIcon />
         </IconButton>
       </StyledLetterPaperTop>
-      <StyledMessageList>
-        {messageList.map((message) => (
-          <Link key={message.id} to={`message/${message.id}`}>
-            <RollingpaperMessage
-              content={message.content}
-              author={message.from}
-            />
-          </Link>
+      <StyledSlicedMessageLists>
+        {slicedMessageLists.map((messageList, index) => (
+          <StyledMessageList key={index}>
+            {messageList.map((message) => (
+              <Link key={message.id} to={`message/${message.id}`}>
+                <RollingpaperMessage
+                  content={message.content}
+                  author={message.from}
+                />
+              </Link>
+            ))}
+          </StyledMessageList>
         ))}
-      </StyledMessageList>
+      </StyledSlicedMessageLists>
     </StyledLetterPaper>
   );
 };
@@ -69,6 +108,16 @@ const StyledTo = styled.h3`
 `;
 
 const StyledMessageList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+
+  a {
+    display: inline-block;
+  }
+`;
+
+const StyledSlicedMessageLists = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
   grid-row-gap: 20px;
