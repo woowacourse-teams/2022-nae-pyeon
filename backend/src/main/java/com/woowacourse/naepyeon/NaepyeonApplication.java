d23f0f5fc2dc692a304cfa315f3e07fe39ddd7ec
kth990303 <kth990303@naver.com>
2022-07-26 16:05:40 +0900

    refactor: separate auditing configuration with default application

--- backend/src/main/java/com/woowacourse/naepyeon/NaepyeonApplication.java
+++ backend/src/main/java/com/woowacourse/naepyeon/NaepyeonApplication.java
@@ -2,9 +2,7 @@ package com.woowacourse.naepyeon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @SpringBootApplication
 public class NaepyeonApplication {
 
