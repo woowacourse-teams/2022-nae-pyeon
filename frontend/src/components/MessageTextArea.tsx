0f4191cf23c22aa3ceb3ea55a63f41482fae761c
soyi47 <soyi.dev@gmail.com>
2022-07-27 18:04:48 +0900

    feat: MessageTextArea 컴포넌트 구현

--- frontend/src/components/MessageTextArea.tsx
+++ frontend/src/components/MessageTextArea.tsx
@@ -1,39 +1,97 @@
 import React, { useRef } from "react";
 import styled from "@emotion/styled";
 
-export const MessageTextArea = () => {
+type MessageTextAreaProps = {
+  backgroundColor: string;
+} & React.TextareaHTMLAttributes<HTMLTextAreaElement>;
+
+type StyledMessageContainerProps = {
+  backgroundColor: string;
+};
+
+export const MessageTextArea = ({
+  value,
+  onChange,
+  children,
+  placeholder,
+  backgroundColor,
+}: MessageTextAreaProps) => {
   const textareaRef = useRef(null);
 
-  const handleTextAreaChange = () => {
+  const handleTextAreaChange: React.ChangeEventHandler<HTMLTextAreaElement> = (
+    e
+  ) => {
+    if (onChange) {
+      onChange(e);
+    }
+
     const textarea = textareaRef.current as unknown as HTMLTextAreaElement;
     textarea.style.height = "auto";
     textarea.style.height = `${textarea.scrollHeight}px`;
   };
 
   return (
-    <StyledMessage ref={textareaRef} onChange={handleTextAreaChange}>
-      MessageWrite
-    </StyledMessage>
+    <StyledMessageContainer backgroundColor={backgroundColor}>
+      <StyledTextArea
+        ref={textareaRef}
+        value={value}
+        onChange={handleTextAreaChange}
+        placeholder={placeholder}
+        maxLength={500}
+      >
+        {children}
+      </StyledTextArea>
+      <StyledTextLength>{(value as string).length}/500</StyledTextLength>
+    </StyledMessageContainer>
   );
 };
 
-const StyledMessage = styled.textarea`
+const StyledMessageContainer = styled.div<StyledMessageContainerProps>`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  white-space: pre-line;
-  overflow: hidden;
 
   width: 130px;
   min-height: 130px;
-  padding: 20px 15px;
+  padding: 20px 15px 10px;
 
-  background-color: ${({ theme }) => theme.colors.YELLOW_300};
+  font-size: 14px;
+  line-height: 16px;
+
+  border: 2px solid ${({ theme }) => theme.colors.GRAY_700};
+  background-color: ${({ backgroundColor, theme }) =>
+    backgroundColor || theme.colors.YELLOW_300};
 
   @media only screen and (min-width: 600px) {
     width: 180px;
     min-height: 180px;
+
+    font-size: 14px;
+    line-height: 18px;
   }
+`;
+
+const StyledTextArea = styled.textarea`
+  width: 100%;
+
+  border: none;
+  background-color: transparent;
+
+  resize: none;
+
+  font-size: inherit;
+  line-height: inherit;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const StyledTextLength = styled.div`
+  display: inline;
+  align-self: flex-end;
+
+  color: ${({ theme }) => theme.colors.GRAY_600};
 `;
 
 export default MessageTextArea;
