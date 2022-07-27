66cce8f9b6767bdc5fd284048c340ad4fdd3bcf8
soyi47 <soyi.dev@gmail.com>
2022-07-27 21:37:21 +0900

    feat: MessateTextArea 스타일 추가

--- frontend/src/components/MessageTextArea.tsx
+++ frontend/src/components/MessageTextArea.tsx
@@ -73,6 +73,7 @@ const StyledMessageContainer = styled.div<StyledMessageContainerProps>`
 
 const StyledTextArea = styled.textarea`
   width: 100%;
+  height: 100%;
 
   border: none;
   background-color: transparent;
