14e5e836b5520a50b67e143e41e764ba208b3103
soyi47 <soyi.dev@gmail.com>
2022-07-28 14:19:06 +0900

    refactor: 메서드의 사용하지 않는 인자 제거
    handleMessageWriteButtonClick의 e 제거

--- frontend/src/components/LetterPaper.tsx
+++ frontend/src/components/LetterPaper.tsx
@@ -23,7 +23,7 @@ const LetterPaper = ({ to, messageList }: LetterPaperProp) => {
 
   const handleMessageWriteButtonClick: React.MouseEventHandler<
     HTMLButtonElement
-  > = (e) => {
+  > = () => {
     setWriteNewMessage(true);
   };
 
