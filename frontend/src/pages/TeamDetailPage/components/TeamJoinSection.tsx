e2a89d2056824599a7d50ae27f47f4a7843a4e24
SunHo Park <67692759+prefer2@users.noreply.github.com>
2022-07-31 16:26:17 +0900

    feat: 파일 및 폴더구조와 라우팅 정리 (#223)
    * refactor: pages 폴더 정리 및 불필요한 페이지 제거
    
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>
    
    * refactor: component 폴더 정리 및 불필요한 component 제거
    
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>
    
    * refactor: component명 변경 및 storybook 폴더 정리
    
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>
    
    Co-authored-by: Soyi Jeon <soyi.dev@gmail.com>

---
+++ frontend/src/pages/TeamDetailPage/components/TeamJoinSection.tsx
@@ -0,0 +1,144 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+import RollingpaperListItem from "@/pages/TeamDetailPage/components/RollingpaperListItem";
+import IconButton from "@/components/IconButton";
+import LineButton from "@/components/LineButton";
+import TeamJoinModalForm from "@/pages/TeamDetailPage/components/TeamJoinModalForm";
+
+import PlusIcon from "@/assets/icons/bx-plus.svg";
+
+const dummyRollingpapers = [
+  {
+    id: 1,
+    title: "우테코 고마워",
+    to: "우아한테크코스",
+  },
+  {
+    id: 2,
+    title: "오늘의 내 편 데일리 미팅",
+    to: "내 편",
+  },
+  {
+    id: 3,
+    title: "이번 주 우리의 한 마디",
+    to: "우아한테크코스",
+  },
+];
+
+const TeamJoinSection = () => {
+  const [isOpenJoinForm, setIsOpenJoinForm] = useState(false);
+
+  const handleJoinFormCloseButtonClick = () => {
+    setIsOpenJoinForm(false);
+  };
+
+  return (
+    <StyledRollingpaperListContainer>
+      <StyledDimmer />
+      <StyledTeamJoinModal>
+        <p>롤링페이퍼를 확인하려면 모임에 참여해주세요</p>
+        <LineButton
+          onClick={() => {
+            setIsOpenJoinForm(true);
+          }}
+        >
+          참여 요청하기
+        </LineButton>
+      </StyledTeamJoinModal>
+      <StyledRollingpaperListHead>
+        <h4>롤링페이퍼 목록</h4>
+        <IconButton size="small">
+          <PlusIcon />
+        </IconButton>
+      </StyledRollingpaperListHead>
+      <StyledRollingpaperList>
+        {dummyRollingpapers.map((rollingpaper) => (
+          <RollingpaperListItem key={rollingpaper.id} {...rollingpaper} />
+        ))}
+      </StyledRollingpaperList>
+      {isOpenJoinForm && (
+        <TeamJoinModalForm
+          onClickCloseButton={handleJoinFormCloseButtonClick}
+        />
+      )}
+    </StyledRollingpaperListContainer>
+  );
+};
+
+const StyledDimmer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  transform: translate(calc(-5%), calc(-5%));
+
+  width: 110%;
+  height: 110%;
+
+  background-color: ${({ theme }) => `${theme.colors.GRAY_200}29`};
+  filter: blur(10px);
+  backdrop-filter: blur(8px);
+`;
+
+const StyledTeamJoinModal = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+
+  display: flex;
+  word-break: keep-all;
+  flex-direction: column;
+  justify-content: space-between;
+
+  width: 240px;
+  height: 150px;
+  padding: 20px 16px;
+
+  text-align: center;
+
+  box-shadow: 0px 4px 4px 2px rgba(147, 147, 147, 0.25);
+  border-radius: 8px;
+
+  background-color: ${({ theme }) => theme.colors.WHITE};
+
+  @media only screen and (min-width: 960px) {
+    width: 50%;
+    height: 50%;
+  }
+
+  p {
+    margin: auto;
+    font-size: 18px;
+  }
+`;
+
+const StyledRollingpaperListContainer = styled.div`
+  position: relative;
+  width: 80%;
+`;
+
+const StyledRollingpaperListHead = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+
+  padding: 16px 0;
+
+  h4 {
+    font-size: 20px;
+    font-weight: bold;
+  }
+`;
+
+const StyledRollingpaperList = styled.ul`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
+`;
+
+export default TeamJoinSection;
---
+++ frontend/src/pages/TeamDetailPage/index.tsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams } from "react-router-dom";
+import styled from "@emotion/styled";
+import axios from "axios";
+
+import appClient from "@/api";
+
+import RollingpaperList from "@/pages/TeamDetailPage/components/RollingpaperList";
+import TeamJoinSection from "@/pages/TeamDetailPage/components/TeamJoinSection";
+
+import { CustomError } from "@/types";
+interface Team {
+  id: number;
+  name: string;
+  description: string;
+  emoji: string;
+  color: string;
+  joined: boolean;
+}
+
+const TeamDetailPage = () => {
+  const { teamId } = useParams();
+
+  const {
+    isLoading: isLoadingTeamDetail,
+    isError: isErrorTeamDetail,
+    error: TeamDetailError,
+    data: teamDetail,
+  } = useQuery<Team>(["team"], () =>
+    appClient.get(`/teams/${teamId}`).then((response) => response.data)
+  );
+
+  if (isLoadingTeamDetail) {
+    return <div>로딩중</div>;
+  }
+
+  if (isErrorTeamDetail) {
+    if (axios.isAxiosError(TeamDetailError) && TeamDetailError.response) {
+      const customError = TeamDetailError.response.data as CustomError;
+      return <div>{customError.message}</div>;
+    }
+    return <div>에러</div>;
+  }
+
+  if (!teamDetail) {
+    return <div>에러</div>;
+  }
+
+  return (
+    <StyledMain>
+      <TeamDescriptionBox
+        emoji={teamDetail.emoji}
+        name={teamDetail.name}
+        description={teamDetail.description}
+        color={teamDetail.color}
+      />
+      {teamDetail.joined ? <RollingpaperList /> : <TeamJoinSection />}
+    </StyledMain>
+  );
+};
+
+interface TeamDescriptionBoxProps {
+  name: string;
+  description: string;
+  emoji: string;
+  color: string;
+}
+
+type StyledTeamDescriptionContainerProps = Pick<
+  TeamDescriptionBoxProps,
+  "color"
+>;
+
+const TeamDescriptionBox = ({
+  name,
+  description,
+  emoji,
+  color,
+}: TeamDescriptionBoxProps) => {
+  return (
+    <StyledTeamDescriptionContainer color={color}>
+      <h3>{`${emoji} ${name}`}</h3>
+      <p>{description}</p>
+    </StyledTeamDescriptionContainer>
+  );
+};
+
+const StyledMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 40px;
+
+  padding: 28px 0;
+`;
+
+const StyledTeamDescriptionContainer = styled.div<StyledTeamDescriptionContainerProps>`
+  width: 80%;
+
+  padding: 28px 16px;
+  border-radius: 8px;
+  background-color: ${({ color }) => `${color}AB`};
+
+  h3 {
+    font-size: 32px;
+    margin-bottom: 10px;
+  }
+`;
+
+export default TeamDetailPage;
