072ff445353c50f030e1134b717ee31708b5413b
soyi47 <soyi.dev@gmail.com>
2022-08-01 14:40:06 +0900

    feat: 로그인 페이지 UI 수정
    - 일반 로그인 관련 UI 및 로직 제거
    - 페이지 레이아웃과 소셜 로그인 버튼 정렬 추가 및 로고 사이즈 수정

--- frontend/src/pages/LoginPage/components/Logo.tsx
+++ frontend/src/pages/LoginPage/components/Logo.tsx
@@ -22,11 +22,22 @@ const StyledLogo = styled.div`
     width: 56px;
     height: 60px;
     margin-right: 10px;
+
+    @media only screen and (min-width: 960px) {
+      width: 84px;
+      height: 90px;
+      margin-right: 15px;
+    }
   }
 
   h1 {
     margin-top: 8px;
     font-size: 56px;
+
+    @media only screen and (min-width: 960px) {
+      margin-top: 12px;
+      font-size: 84px;
+    }
   }
 `;
 
