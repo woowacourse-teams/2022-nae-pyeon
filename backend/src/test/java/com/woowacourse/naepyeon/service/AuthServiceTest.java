39bb69bef84e078c02c30a1e53792fc6b794be33
yxxnghwan <younghwan960@gmail.com>
2022-08-02 14:00:22 +0900

    test: AuthService.createToken()메서드 테스트 작성

--- backend/src/test/java/com/woowacourse/naepyeon/service/AuthServiceTest.java
+++ backend/src/test/java/com/woowacourse/naepyeon/service/AuthServiceTest.java
@@ -1,6 +1,16 @@
 package com.woowacourse.naepyeon.service;
 
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.woowacourse.naepyeon.domain.Platform;
+import com.woowacourse.naepyeon.service.dto.MemberResponseDto;
+import com.woowacourse.naepyeon.service.dto.TokenRequestDto;
+import com.woowacourse.naepyeon.service.dto.TokenResponseDto;
 import com.woowacourse.naepyeon.support.JwtTokenProvider;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,4 +28,29 @@ class AuthServiceTest {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    @Test
+    @DisplayName("사용자 정보를 받아 토큰을 만들어 반환한다.")
+    void createToken() {
+        final TokenRequestDto tokenRequestDto =
+                new TokenRequestDto(Platform.KAKAO.name(), "1", "email@email.com", "alex", "url");
+
+        final TokenResponseDto tokenResponseDto = authService.createToken(tokenRequestDto);
+
+        assertThatCode(() -> jwtTokenProvider.validateAbleToken(tokenResponseDto.getAccessToken()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("사용자 정보가 DB에 없는 경우 DB에 사용자 정보를 추가하고 토큰을 반환한다.")
+    void createTokenWithNotExistMember() {
+        final TokenRequestDto tokenRequestDto =
+                new TokenRequestDto(Platform.KAKAO.name(), "1", "email@email.com", "alex", "url");
+        final TokenResponseDto tokenResponseDto = authService.createToken(tokenRequestDto);
+
+        final Long memberId = tokenResponseDto.getId();
+        final MemberResponseDto memberResponse = memberService.findById(memberId);
+
+        assertThat(memberResponse).extracting("id", "username", "email")
+                .containsExactly(memberId, tokenRequestDto.getUsername(), tokenRequestDto.getEmail());
+    }
 }
