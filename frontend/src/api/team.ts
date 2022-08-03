431b2dea13f9ba24108600fee75f3c53989a7577
soyi47 <soyi.dev@gmail.com>
2022-08-03 20:31:05 +0900

    fix: getMyTeams의 pageParam 초기값 수정
    - getMyTeams pageParam 초기값 1 -> 0으로 변경

--- frontend/src/api/team.ts
+++ frontend/src/api/team.ts
@@ -2,7 +2,7 @@ import { appClient } from "@/api";
 
 const getMyTeams =
   (teamPageCount = 5) =>
-  async ({ pageParam = 1 }) => {
+  async ({ pageParam = 0 }) => {
     const data = appClient
       .get(`teams/me?page=${pageParam}&count=${teamPageCount}`)
       .then((response) => response.data);
