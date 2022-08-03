0232b68d75704568511e797d7c6d71f08f0b8c39
Soyi Jeon <soyi.dev@gmail.com>
2022-08-03 21:18:46 +0900

    fix: 페이지네이션이 적용된 API 요청에서 발생하는 오류 해결 (#280)
    * fix: getMyTeams의 pageParam 초기값 수정
    
    - getMyTeams pageParam 초기값 1 -> 0으로 변경
    
    * refactor: 모임 검색 결과 요청 API 파일 분리
    
    * refactor: 모임 검색 요청 query key 변경
    
    - (기존) ["projects"] -> (수정) ["team-search"]
    
    * refactor: 모임 검색 요청 query key 변경
    
    query key 배열에 searchKeyword 추가

--- frontend/src/api/team.ts
+++ frontend/src/api/team.ts
@@ -2,11 +2,20 @@ import { appClient } from "@/api";
 
 const getMyTeams =
   (teamPageCount = 5) =>
-  async ({ pageParam = 1 }) => {
+  async ({ pageParam = 0 }) => {
     const data = appClient
       .get(`teams/me?page=${pageParam}&count=${teamPageCount}`)
       .then((response) => response.data);
     return data;
   };
 
-export { getMyTeams };
+const getTeamSearchResult =
+  ({ keyword, count }: { keyword: string; count: number }) =>
+  async ({ pageParam = 0 }) => {
+    const data = appClient
+      .get(`teams?keyword=${keyword}&page=${pageParam}&count=${count}`)
+      .then((response) => response.data);
+    return data;
+  };
+
+export { getMyTeams, getTeamSearchResult };
