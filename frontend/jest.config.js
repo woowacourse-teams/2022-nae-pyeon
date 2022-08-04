98e0b756bd307e74045843497f0ae6c8790c218e
soyi47 <soyi.dev@gmail.com>
2022-08-05 05:32:05 +0900

    chore: jest.config.js 업데이트
    - svg 파일 관련 에러 해결을 위해 transform 옵션 업데이트
    - testRegex 옵션 설정
    - fileTransformer.js 추가

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -2,8 +2,11 @@ module.exports = {
   transform: {
     "^.+\\.tsx?$": "ts-jest",
     "^.+\\.js$": "babel-jest",
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+      "<rootDir>/__tests__/config_util/fileTransformer.js",
   },
   collectCoverage: false,
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   moduleFileExtensions: ["jsx", "tsx", "js", "json", "node", "ts", "svg"],
   collectCoverageFrom: ["src/**/*.{js,jsx,ts,tsx}"],
   testEnvironment: "jsdom",
