195fe3b57ab640f7de656cdb27d9516a037844ad
soyi47 <soyi.dev@gmail.com>
2022-08-11 00:47:45 +0900

    feat: MessageTextArea 스타일 변경
    - padding 값 변경
    - border 제거, box-shadow 추가

--- frontend/src/pages/RollingpaperPage/components/MessageTextArea.tsx
+++ frontend/src/pages/RollingpaperPage/components/MessageTextArea.tsx
@@ -60,18 +60,20 @@ const StyledMessageContainer = styled.div<StyledMessageContainerProps>`
   width: 100%;
   aspect-ratio: 1;
   min-width: 180px;
-  padding: 20px 15px 10px;
+  padding: 20px 20px 68px;
 
   font-size: 16px;
   line-height: 22px;
 
-  border: 2px solid ${({ theme }) => theme.colors.GRAY_700};
   background-color: ${({ backgroundColor }) => backgroundColor};
+  box-shadow: inset 2px 2px ${({ theme }) => theme.colors.GRAY_700},
+    inset -2px -2px ${({ theme }) => theme.colors.GRAY_700};
 `;
 
 const StyledTextArea = styled.textarea`
   width: 100%;
   height: 100%;
+  padding: 0;
 
   border: none;
   background-color: transparent;
