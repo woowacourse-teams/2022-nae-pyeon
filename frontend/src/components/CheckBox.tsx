23688212ba16e662a49f8b558518f17b12fbff37
soyi47 <soyi.dev@gmail.com>
2022-08-11 13:39:11 +0900

    fix: CheckBox 스타일 수정

--- frontend/src/components/CheckBox.tsx
+++ frontend/src/components/CheckBox.tsx
@@ -19,16 +19,19 @@ const StyledCheckBoxInput = styled.input`
 
   width: 20px;
   height: 20px;
+  font-size: 14px;
 
-  border: 2px solid ${({ theme }) => theme.colors.SKY_BLUE_300};
+  border: 2px solid ${({ theme }) => theme.colors.GRAY_600};
   border-radius: 2px;
-  background-color: ${({ theme }) => theme.colors.GRAY_100};
+  background-color: transparent;
 
   &:hover {
     cursor: pointer;
   }
 
   &:checked {
+    border: 2px solid ${({ theme }) => theme.colors.GRAY_800};
+
     background-position: 50% 45%;
     background-repeat: no-repeat;
     background-image: url("data:image/svg+xml,%3Csvg width='13px' viewBox='0 0 23 17' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2 7L9.11069 14.1107L21.8318 1.38956' stroke='%23242424' stroke-width='3'/%3E%3C/svg%3E%0A");
