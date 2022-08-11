13cbf048fd477e285bde98f6f16ca6502c2c4c04
soyi47 <soyi.dev@gmail.com>
2022-08-11 13:39:27 +0900

    refactor: LabeledCheckBox 내에서 CheckBox 사용하도록 수정

--- frontend/src/components/LabeledCheckBox.tsx
+++ frontend/src/components/LabeledCheckBox.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from "@emotion/styled";
+import CheckBox from "@/components/CheckBox";
 
 type LabeledCheckBoxProps = {
   labeledText: string;
@@ -16,11 +17,7 @@ const LabeledCheckBox = ({
 }: LabeledCheckBoxProps) => {
   return (
     <StyledLabel checked={checked}>
-      <StyledCheckBoxInput
-        type="checkbox"
-        checked={checked}
-        onChange={onChange}
-      />
+      <CheckBox type="checkbox" checked={checked} onChange={onChange} />
       {labeledText}
     </StyledLabel>
   );
@@ -33,30 +30,6 @@ const StyledLabel = styled.label<StyledLabelProps>`
 
   color: ${({ checked, theme }) =>
     checked ? theme.colors.GRAY_800 : theme.colors.GRAY_600};
-`;
-
-const StyledCheckBoxInput = styled.input`
-  appearance: none;
-
-  width: 20px;
-  height: 20px;
-  font-size: 14px;
-
-  border: 2px solid ${({ theme }) => theme.colors.GRAY_600};
-  border-radius: 2px;
-  background-color: transparent;
-
-  &:hover {
-    cursor: pointer;
-  }
-
-  &:checked {
-    border: 2px solid ${({ theme }) => theme.colors.GRAY_800};
-
-    background-position: 50% 45%;
-    background-repeat: no-repeat;
-    background-image: url("data:image/svg+xml,%3Csvg width='13px' viewBox='0 0 23 17' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M2 7L9.11069 14.1107L21.8318 1.38956' stroke='%23242424' stroke-width='3'/%3E%3C/svg%3E%0A");
-  }
 `;
 
 export default LabeledCheckBox;
