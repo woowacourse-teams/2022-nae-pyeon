a52623dba2c28b15f624974fbc406abd3768a2c0
soyi47 <soyi.dev@gmail.com>
2022-08-15 14:54:08 +0900

    fix: useCreateInviteLink의 onError 제거

--- frontend/src/pages/TeamDetailPage/hooks/useCreateInviteLink.tsx
+++ frontend/src/pages/TeamDetailPage/hooks/useCreateInviteLink.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import { useMutation } from "@tanstack/react-query";
 
-import axios from "axios";
-
 import { postTeamInviteToken } from "@/api/team";
 
-import { CustomError, Team } from "@/types";
+import { Team } from "@/types";
 
 const useCreateInviteLink = () => {
   const {
@@ -14,19 +12,9 @@ const useCreateInviteLink = () => {
     isSuccess,
     isError,
     data,
-  } = useMutation(
-    ({ id }: Pick<Team, "id">) => {
-      return postTeamInviteToken({ id });
-    },
-    {
-      onError: (error) => {
-        if (axios.isAxiosError(error) && error.response) {
-          const customError = error.response.data as CustomError;
-          alert(customError.message);
-        }
-      },
-    }
-  );
+  } = useMutation(({ id }: Pick<Team, "id">) => {
+    return postTeamInviteToken({ id });
+  });
 
   return { createInviteLink, isLoading, isSuccess, isError, data };
 };
