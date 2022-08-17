e78fb89a6b7d32678925db8e4e504e1b0294ccc9
kth990303 <kth990303@naver.com>
2022-08-17 17:33:52 +0900

    fix: 버그 수정

--- backend/src/main/java/com/woowacourse/naepyeon/service/dto/AppleDto.java
+++ backend/src/main/java/com/woowacourse/naepyeon/service/dto/AppleDto.java
@@ -1,13 +1,5 @@
 package com.woowacourse.naepyeon.service.dto;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-
-@Getter
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
-@AllArgsConstructor
 public class AppleDto {
 
 
