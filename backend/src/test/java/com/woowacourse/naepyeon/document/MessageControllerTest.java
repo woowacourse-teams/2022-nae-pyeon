be46332a350be1e1e2ee0b2bdb6a7260c2d74b78
Kim Seunglae <obey1342@gmail.com>
2022-08-18 16:04:56 +0900

    test: restDocs 수정

--- backend/src/test/java/com/woowacourse/naepyeon/document/MessageControllerTest.java
+++ backend/src/test/java/com/woowacourse/naepyeon/document/MessageControllerTest.java
@@ -6,6 +6,8 @@ import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuild
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.woowacourse.naepyeon.controller.dto.MessageRequest;
+import com.woowacourse.naepyeon.controller.dto.MessageUpdateRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
@@ -13,11 +15,13 @@ class MessageControllerTest extends TestSupport {
 
     @Test
     void createMessage() throws Exception {
+        final MessageRequest messageRequest =
+                new MessageRequest("오늘 하루도 고생했어!\n 내일은 더 좋은 일들이 기다릴거야!", "#123456", false, false);
         mockMvc.perform(
                         post("/api/v1/rollingpapers/{rollingpaperId}/messages", rollingpaperId1)
                                 .header("Authorization", "Bearer " + joinedMemberAccessToken)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(readJson("/json/messages/message-create.json"))
+                                .content(objectMapper.writeValueAsString(messageRequest))
                 )
                 .andExpect(status().isCreated())
                 .andDo(restDocs.document());
@@ -35,11 +39,12 @@ class MessageControllerTest extends TestSupport {
 
     @Test
     void updateMessage() throws Exception {
+        final MessageUpdateRequest messageUpdateRequest = new MessageUpdateRequest("내편팀 파이팅", "#234567", true, false);
         mockMvc.perform(
                         put("/api/v1/rollingpapers/{rollingpaperId}/messages/{messageId}", rollingpaperId1, messageId)
                                 .header("Authorization", "Bearer " + joinedMemberAccessToken)
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(readJson("/json/messages/message-update.json"))
+                                .content(objectMapper.writeValueAsString(messageUpdateRequest))
                 )
                 .andExpect(status().isNoContent())
                 .andDo(restDocs.document());
