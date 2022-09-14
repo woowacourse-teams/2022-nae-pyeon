07b7035e69acf1c9a3bd169212c82e45c6cea0c7
soyi47 <soyi.dev@gmail.com>
2022-09-15 02:38:23 +0900

    feat: member api 함수에 throw ApiError 추가

--- frontend/src/api/member.ts
+++ frontend/src/api/member.ts
@@ -1,35 +1,79 @@
-import { appClient } from "@/api";
+import axios from "axios";
 
-const getMyInfo = () => {
-  return appClient.get("/members/me").then((response) => response.data);
+import { appClient } from "@/api";
+import ApiError from "@/util/ApiError";
+
+const getMyInfo = async () => {
+  try {
+    const { data } = await appClient.get("/members/me");
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
 };
 
-const getMyInfoWithAccessToken = (accessToken: string | null) => {
-  return appClient
-    .get("/members/me", {
+const getMyInfoWithAccessToken = async (accessToken: string | null) => {
+  try {
+    const { data } = await appClient.get("/members/me", {
       headers: {
         Authorization: `Bearer ${accessToken || ""}`,
       },
-    })
-    .then((response) => response.data);
+    });
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
 };
 
-const getMyReceivedRollingpapers = (page = 0, count = 5) => {
-  return appClient
-    .get(`/members/me/rollingpapers/received?page=${page}&count=${count}`)
-    .then((response) => response.data);
+const getMyReceivedRollingpapers = async (page = 0, count = 5) => {
+  try {
+    const { data } = await appClient.get(
+      `/members/me/rollingpapers/received?page=${page}&count=${count}`
+    );
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
 };
 
-const getMySentMessage = (page = 0, count = 5) => {
-  return appClient
-    .get(`/members/me/messages/written?page=${page}&count=${count}`)
-    .then((response) => response.data);
+const getMySentMessage = async (page = 0, count = 5) => {
+  try {
+    const { data } = await appClient.get(
+      `/members/me/messages/written?page=${page}&count=${count}`
+    );
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
 };
 
-const putMyNickname = (username: string) => {
-  return appClient
-    .put("/members/me", { username })
-    .then((response) => response.data);
+const putMyNickname = async (username: string) => {
+  try {
+    const { data } = await appClient.put("/members/me", { username });
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
 };
 
 export {
