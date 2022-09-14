fc1f35270fc40e04edb60f4957f809c11085b434
soyi47 <soyi.dev@gmail.com>
2022-09-15 02:42:53 +0900

    feat: rollingpaper api 함수에 throw ApiError 추가

--- frontend/src/api/rollingpaper.ts
+++ frontend/src/api/rollingpaper.ts
@@ -1,35 +1,67 @@
+import axios from "axios";
+
 import { appClient } from "@/api";
+import ApiError from "@/util/ApiError";
 
 interface RequestPostRollingpaper {
   teamId: number;
   title: string;
   addresseeId: number;
 }
-const getRollingpaper = (teamId: number, id: number) =>
-  appClient
-    .get(`/teams/${teamId}/rollingpapers/${id}`)
-    .then((response) => response.data);
+const getRollingpaper = async (teamId: number, id: number) => {
+  try {
+    const { data } = await appClient.get(
+      `/teams/${teamId}/rollingpapers/${id}`
+    );
 
-const postTeamRollingpaper = ({
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
+};
+
+const postTeamRollingpaper = async ({
   teamId,
   title,
-}: Omit<RequestPostRollingpaper, "addresseeId">) =>
-  appClient
-    .post(`/teams/${teamId}/team-rollingpapers`, {
-      title,
-    })
-    .then((response) => response.data);
+}: Omit<RequestPostRollingpaper, "addresseeId">) => {
+  try {
+    const { data } = await appClient.post(
+      `/teams/${teamId}/team-rollingpapers`,
+      {
+        title,
+      }
+    );
 
-const postMemberRollingpaper = ({
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
+};
+
+const postMemberRollingpaper = async ({
   teamId,
   title,
   addresseeId,
-}: RequestPostRollingpaper) =>
-  appClient
-    .post(`/teams/${teamId}/rollingpapers`, {
+}: RequestPostRollingpaper) => {
+  try {
+    const { data } = await appClient.post(`/teams/${teamId}/rollingpapers`, {
       title,
       addresseeId,
-    })
-    .then((response) => response.data);
+    });
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
+};
 
 export { getRollingpaper, postTeamRollingpaper, postMemberRollingpaper };
