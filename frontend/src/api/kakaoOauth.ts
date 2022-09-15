4d606b2c8db939fda056e2d78f926862d31a4747
soyi47 <soyi.dev@gmail.com>
2022-09-15 11:03:29 +0900

    feat: kakaoOauth api 함수에 throw ApiError 추가

--- frontend/src/api/kakaoOauth.ts
+++ frontend/src/api/kakaoOauth.ts
@@ -1,4 +1,7 @@
+import axios from "axios";
+
 import { appClient } from "@/api";
+import ApiError from "@/util/ApiError";
 
 import { RequestKakaoOauthBody } from "@/types/oauth";
 
@@ -6,11 +9,19 @@ const postKakaoOauth = async ({
   authorizationCode,
   redirectUri,
 }: RequestKakaoOauthBody) => {
-  const response = await appClient.post("/oauth/kakao", {
-    authorizationCode,
-    redirectUri,
-  });
-  return response.data;
+  try {
+    const { data } = await appClient.post("/oauth/kakao", {
+      authorizationCode,
+      redirectUri,
+    });
+
+    return data;
+  } catch (error) {
+    if (axios.isAxiosError(error) && error.response) {
+      const customError = error.response.data as ApiError;
+      throw new ApiError(customError.errorCode, customError.message);
+    }
+  }
 };
 
 export { postKakaoOauth };
