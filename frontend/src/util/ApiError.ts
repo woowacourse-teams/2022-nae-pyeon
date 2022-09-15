54dd01e16c44f5ca8f14a6dc2c7b365b288ce6ff
soyi47 <soyi.dev@gmail.com>
2022-09-15 11:07:21 +0900

    refactor: ApiError에 errorHandler 추가

--- frontend/src/util/ApiError.ts
+++ frontend/src/util/ApiError.ts
@@ -1,10 +1,14 @@
+type ErrorHandler = () => void;
+
 export default class ApiError extends Error {
   errorCode: number;
   message: string;
+  errorHandler: ErrorHandler | undefined;
 
-  constructor(errorCode: number, message: string) {
+  constructor(errorCode: number, message: string, errorHandler?: ErrorHandler) {
     super(message);
     this.errorCode = errorCode;
     this.message = message;
+    this.errorHandler = errorHandler;
   }
 }
