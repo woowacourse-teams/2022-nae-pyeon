2d84ab91ae3ed41330470dc88d1e5a43103cd1fa
Soyi Jeon <soyi.dev@gmail.com>
2022-09-15 13:44:53 +0900

    refactor: 모임 추가하기 페이지 개선 (#424)
    * fix: 모임 이름 정규식 수정
    
    모든 문자 허용하도록 변경
    
    * refactor: LabeledTextArea 오타 수정 및 불필요한 props 명시 제거
    
    * feat: LabeledTextArea 글자 수 보여주기 구현 및 크기 조정
    
    * refactor: TeamCreationPage의 모임 설명 Input prop 수정
    
    * fix: 모임설명 Input labelText, maxLength, placeholder 변경
    
    * fix: 모임 한 줄 소개 입력란을 LabeledInput으로 변경

--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -4,8 +4,9 @@ const REGEX = {
   EMAIL: /^[_a-z0-9-]+(.[_a-z0-9-]+)*@(?:\w+\.)+\w+$/,
   USERNAME: /^.{1,64}$/,
   PASSWORD: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d~!@#$%^&*()+|=]{8,20}$/,
-  TEAM_NAME: /^[가-힣a-zA-Z\d~!@#$%^&*()+_\-\s]{1,20}$/,
+  TEAM_NAME: /^.{1,20}$/,
   TEAM_NICKNAME: /^.{1,20}$/,
+  TEAM_DESCRIPTION: /^.{1,80}$/,
   ROLLINGPAPER_TITLE: /^.{1,20}$/,
 };
 
