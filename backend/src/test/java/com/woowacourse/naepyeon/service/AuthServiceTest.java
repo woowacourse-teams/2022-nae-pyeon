c74aa6c264a0f4e146855f924a54b4acf64e7c52
asebn1 <asebn121@gmail.com>
2022-09-20 05:29:19 +0900

    test: 서비스, 컨트롤러 테스트 추가

--- backend/src/test/java/com/woowacourse/naepyeon/service/AuthServiceTest.java
+++ backend/src/test/java/com/woowacourse/naepyeon/service/AuthServiceTest.java
@@ -1,10 +1,5 @@
 package com.woowacourse.naepyeon.service;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.mockito.Mockito.anyString;
-import static org.mockito.Mockito.when;
-
 import com.woowacourse.naepyeon.domain.Member;
 import com.woowacourse.naepyeon.domain.Platform;
 import com.woowacourse.naepyeon.repository.MemberRepository;
@@ -21,6 +16,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.mockito.Mockito.anyString;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Transactional
@@ -48,8 +48,8 @@ class AuthServiceTest {
     }
 
     @Test
-    @DisplayName("사용자 정보를 받아 토큰을 만들어 반환한다.")
-    void createToken() {
+    @DisplayName("카카오 로그인 정보를 받아 토큰을 만들어 반환한다.")
+    void createTokenByKakao() {
         final PlatformUserDto platformUserDto = new PlatformUserDto("alex", "email@email.com", "KAKAO", "1");
         when(kakaoPlatformUserProvider.getPlatformUser(anyString(), anyString())).thenReturn(platformUserDto);
 
@@ -61,13 +61,48 @@ class AuthServiceTest {
     }
 
     @Test
-    @DisplayName("사용자 정보가 DB에 없는 경우 DB에 사용자 정보를 추가하고 토큰을 반환한다.")
-    void createTokenWithNotExistMember() {
+    @DisplayName("카카오 로그인 정보가 DB에 없는 경우 DB에 사용자 정보를 추가하고 토큰을 반환한다.")
+    void createTokenWithNotExistMemberByKakao() {
         final PlatformUserDto platformUserDto = new PlatformUserDto("alex", "email@email.com", "KAKAO", "1");
         when(kakaoPlatformUserProvider.getPlatformUser(anyString(), anyString())).thenReturn(platformUserDto);
         final TokenRequestDto tokenRequestDto = new TokenRequestDto("authorizationCode", "https://...");
 
         final TokenResponseDto tokenResponseDto = authService.createTokenWithKakaoOauth(tokenRequestDto);
+        final Long memberId = tokenResponseDto.getId();
+        final Member findMember = memberRepository.findById(memberId)
+                .get();
+
+        assertThat(findMember).extracting("id", "username", "email", "platform", "platformId")
+                .containsExactly(
+                        memberId,
+                        platformUserDto.getUsername(),
+                        platformUserDto.getEmail(),
+                        Platform.valueOf(platformUserDto.getPlatform()),
+                        platformUserDto.getPlatformId()
+                );
+    }
+
+    @Test
+    @DisplayName("구글 로그인 정보를 받아 토큰을 만들어 반환한다.")
+    void createTokenByGoogle() {
+        final PlatformUserDto platformUserDto = new PlatformUserDto("zero", "email@email.com", "GOOGLE", "1");
+        when(googlePlatformUserProvider.getPlatformUser(anyString(), anyString())).thenReturn(platformUserDto);
+
+        final TokenRequestDto tokenRequestDto = new TokenRequestDto("authorizationCode", "https://...");
+        final TokenResponseDto tokenResponseDto = authService.createTokenWithGoogleOauth(tokenRequestDto);
+
+        assertThatCode(() -> jwtTokenProvider.validateAbleToken(tokenResponseDto.getAccessToken()))
+                .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("구글 로그인 정보가 DB에 없는 경우 DB에 사용자 정보를 추가하고 토큰을 반환한다.")
+    void createTokenWithNotExistMemberByGoogle() {
+        final PlatformUserDto platformUserDto = new PlatformUserDto("zero", "email@email.com", "GOOGLE", "1");
+        when(googlePlatformUserProvider.getPlatformUser(anyString(), anyString())).thenReturn(platformUserDto);
+        final TokenRequestDto tokenRequestDto = new TokenRequestDto("authorizationCode", "https://...");
+
+        final TokenResponseDto tokenResponseDto = authService.createTokenWithGoogleOauth(tokenRequestDto);
         final Long memberId = tokenResponseDto.getId();
         final Member findMember = memberRepository.findById(memberId)
                 .get();
