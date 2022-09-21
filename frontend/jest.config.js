445b988d3a99d2aa666b0caca1e7fe7ac8cae69b
prefer2 <sunho6200@gmail.com>
2022-09-21 21:05:49 +0900

    refactor: jest config transform 수정

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   transform: {
-    "^.+\\.tsx?$": "babel-jest",
+    "^.+\\.[jt]sx?$": "ts-jest",
+    "^.+\\.js$": "babel-jest",
     "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
       "<rootDir>/__tests__/config_util/fileTransformer.js",
   },
