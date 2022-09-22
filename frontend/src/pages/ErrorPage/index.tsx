23a72c9a79f90c5c3b418f0e172d7fbd38b42b11
SunHo Park <67692759+prefer2@users.noreply.github.com>
2022-09-22 20:45:06 +0900

    feat: error page 수정 (#481)

--- frontend/src/pages/ErrorPage/index.tsx
+++ frontend/src/pages/ErrorPage/index.tsx
@@ -1,7 +1,44 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { useNavigate } from "react-router-dom";
+
+import LineButton from "@/components/LineButton";
+
+import ErrorImg from "@/assets/images/empty-state.svg";
 
 const ErrorPage = () => {
-  return <div>404 Error</div>;
+  const navigate = useNavigate();
+
+  return (
+    <StyledContainer>
+      <ErrorImg />
+      <StyledMessage>에러 발생!</StyledMessage>
+      <LineButton onClick={() => navigate("/", { replace: true })}>
+        홈으로 돌아가기
+      </LineButton>
+    </StyledContainer>
+  );
 };
+
+const StyledContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  margin-top: 100px;
+
+  svg {
+    font-size: 150px;
+  }
+`;
+
+const StyledMessage = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.GRAY_400};
+
+  margin-bottom: 60px;
+`;
 
 export default ErrorPage;
