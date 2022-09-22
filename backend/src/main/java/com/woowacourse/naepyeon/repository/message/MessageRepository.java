122f414d56473a761f0580118e390dddd2abafe1
TaeHyeon Kim <57135043+kth990303@users.noreply.github.com>
2022-09-22 19:42:20 +0900

    fix: 잘못된 querydsl JPQL로 임시로 변환 (#479)

--- backend/src/main/java/com/woowacourse/naepyeon/repository/message/MessageRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/message/MessageRepository.java
@@ -1,8 +1,29 @@
 package com.woowacourse.naepyeon.repository.message;
 
 import com.woowacourse.naepyeon.domain.Message;
+import com.woowacourse.naepyeon.service.dto.WrittenMessageResponseDto;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MessageRepository extends JpaRepository<Message, Long>, MessageRepositoryCustom {
 
+    @Query(value = "select distinct new com.woowacourse.naepyeon.service.dto.WrittenMessageResponseDto"
+            + "(m.id, r.id, r.title, t.id, t.name, m.content, m.color, "
+            + "case when r.recipient = com.woowacourse.naepyeon.domain.rollingpaper.Recipient.MEMBER then p.nickname "
+            + "when r.recipient = com.woowacourse.naepyeon.domain.rollingpaper.Recipient.TEAM then t.name "
+            + "else '' end) "
+            + "from Message m"
+            + ", Rollingpaper r"
+            + ", Team t"
+            + ", TeamParticipation p "
+            + "where m.rollingpaper.id = r.id "
+            + "and r.team.id = t.id "
+            + "and p.team.id = t.id "
+            + "and m.author.id = :authorId "
+            + "and (p.member.id = r.member.id or r.member.id is null)")
+    Page<WrittenMessageResponseDto> findAllByAuthorId(@Param("authorId") final Long authorId,
+                                                      final Pageable pageRequest);
 }
--- backend/src/main/java/com/woowacourse/naepyeon/repository/message/MessageRepositoryImpl.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/message/MessageRepositoryImpl.java
@@ -1,26 +1,14 @@
 package com.woowacourse.naepyeon.repository.message;
 
 import static com.woowacourse.naepyeon.domain.QMessage.message;
-import static com.woowacourse.naepyeon.domain.QTeam.team;
-import static com.woowacourse.naepyeon.domain.QTeamParticipation.teamParticipation;
-import static com.woowacourse.naepyeon.domain.rollingpaper.QRollingpaper.rollingpaper;
 
 import com.querydsl.core.BooleanBuilder;
-import com.querydsl.core.types.ConstructorExpression;
-import com.querydsl.core.types.Projections;
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.CaseBuilder;
-import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.woowacourse.naepyeon.domain.Message;
-import com.woowacourse.naepyeon.domain.rollingpaper.Recipient;
-import com.woowacourse.naepyeon.service.dto.WrittenMessageResponseDto;
 import java.util.List;
 import java.util.function.Supplier;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.support.PageableExecutionUtils;
 
 @RequiredArgsConstructor
 public class MessageRepositoryImpl implements MessageRepositoryCustom {
@@ -35,70 +23,8 @@ public class MessageRepositoryImpl implements MessageRepositoryCustom {
                 .fetch();
     }
 
-    @Override
-    public Page<WrittenMessageResponseDto> findAllByAuthorId(final Long authorId, final Pageable pageRequest) {
-        final JPAQuery<WrittenMessageResponseDto> query = getQueryWhenFindAllByAuthorId(authorId);
-
-        final List<WrittenMessageResponseDto> content = query
-                .offset(pageRequest.getOffset())
-                .limit(pageRequest.getPageSize())
-                .fetch();
-
-        final JPAQuery<Long> countQuery = getCountQueryWhenFindAllByAuthorId(authorId);
-
-        return PageableExecutionUtils.getPage(content, pageRequest, countQuery::fetchOne);
-    }
-
-    private JPAQuery<WrittenMessageResponseDto> getQueryWhenFindAllByAuthorId(final Long authorId) {
-        return queryFactory
-                .select(makeProjections())
-                .distinct()
-                .from(message, rollingpaper, team, teamParticipation)
-                .where(message.rollingpaper.id.eq(rollingpaper.id)
-                        .and(rollingpaper.team.id.eq(team.id))
-                        .and(teamParticipation.id.eq(team.id))
-                        .and(message.author.id.eq(authorId))
-                        .and(teamParticipation.member.id.eq(rollingpaper.id)
-                                .or(rollingpaper.member.id.isNull())));
-    }
-
-    private JPAQuery<Long> getCountQueryWhenFindAllByAuthorId(final Long authorId) {
-        return queryFactory
-                .select(message.count())
-                .distinct()
-                .from(message, rollingpaper, team, teamParticipation)
-                .where(message.rollingpaper.id.eq(rollingpaper.id)
-                        .and(rollingpaper.team.id.eq(team.id))
-                        .and(teamParticipation.id.eq(team.id))
-                        .and(message.author.id.eq(authorId))
-                        .and(teamParticipation.member.id.eq(rollingpaper.id)
-                                .or(rollingpaper.member.id.isNull())));
-    }
-
-    private ConstructorExpression<WrittenMessageResponseDto> makeProjections() {
-        return Projections.constructor(WrittenMessageResponseDto.class,
-                message.id,
-                message.rollingpaper.id,
-                message.rollingpaper.title,
-                message.rollingpaper.team.id,
-                message.rollingpaper.team.name,
-                message.content,
-                message.color,
-                new CaseBuilder()
-                        .when(message.rollingpaper.recipient.eq(Recipient.MEMBER))
-                        .then(teamParticipation.nickname)
-                        .when(message.rollingpaper.recipient.eq(Recipient.TEAM))
-                        .then(message.rollingpaper.team.name)
-                        .otherwise("")
-        );
-    }
-
     private BooleanBuilder isRollingpaperIdEq(final Long rollingpaperId) {
         return nullSafeBuilder(() -> message.rollingpaper.id.eq(rollingpaperId));
-    }
-
-    private BooleanBuilder isAuthorIdEq(final Long authorId) {
-        return nullSafeBuilder(() -> message.author.id.eq(authorId));
     }
 
     private BooleanBuilder nullSafeBuilder(Supplier<BooleanExpression> f) {
