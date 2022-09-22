f0659423416df0a65f8d3c44ef1197952f5ff930
Kim Seunglae <obey1342@gmail.com>
2022-09-22 21:57:43 +0900

    feat: 데이터베이스 이중화 기능 구현

---
+++ backend/src/main/java/com/woowacourse/naepyeon/config/DataSourceConfig.java
@@ -0,0 +1,58 @@
+package com.woowacourse.naepyeon.config;
+
+import com.woowacourse.naepyeon.support.db.DatabaseType;
+import com.woowacourse.naepyeon.support.db.RoutingDataSource;
+import java.util.HashMap;
+import javax.sql.DataSource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@RequiredArgsConstructor
+@ComponentScan(basePackages = {"com.woowacourse.naepyeon"})
+public class DataSourceConfig {
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.source.hikari")
+    public DataSource sourceDataSource() {
+        return DataSourceBuilder.create()
+                .build();
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource.replica.hikari")
+    public DataSource replicaDataSource() {
+        return DataSourceBuilder.create()
+                .build();
+    }
+
+    @Bean
+    public DataSource routingDataSource() {
+        final RoutingDataSource routingDataSource = new RoutingDataSource();
+
+        final HashMap<Object, Object> dataSources = new HashMap<>();
+        dataSources.put(DatabaseType.SOURCE, sourceDataSource());
+        dataSources.put(DatabaseType.REPLICA, sourceDataSource());
+
+        routingDataSource.setDefaultTargetDataSource(sourceDataSource());
+        routingDataSource.setTargetDataSources(dataSources);
+
+        return routingDataSource;
+    }
+
+    @Primary
+    @Bean
+    @DependsOn({"sourceDataSource", "replicaDataSource", "routingDataSource"})
+    public DataSource dataSource() {
+        return new LazyConnectionDataSourceProxy(routingDataSource());
+    }
+}
