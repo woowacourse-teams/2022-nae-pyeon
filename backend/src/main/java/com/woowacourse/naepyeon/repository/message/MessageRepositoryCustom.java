d1973d89677cf9e84268fcdbdf23ab7950c4c398
kth990303 <kth990303@naver.com>
2022-09-22 19:38:29 +0900

    fix: 잘못된 querydsl JPQL로 임시로 변환

--- backend/src/main/java/com/woowacourse/naepyeon/repository/message/MessageRepositoryCustom.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/message/MessageRepositoryCustom.java
@@ -1,14 +1,9 @@
 package com.woowacourse.naepyeon.repository.message;
 
 import com.woowacourse.naepyeon.domain.Message;
-import com.woowacourse.naepyeon.service.dto.WrittenMessageResponseDto;
 import java.util.List;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 public interface MessageRepositoryCustom {
 
     List<Message> findAllByRollingpaperId(final Long rollingpaperId);
-
-    Page<WrittenMessageResponseDto> findAllByAuthorId(final Long authorId, final Pageable pageRequest);
 }
