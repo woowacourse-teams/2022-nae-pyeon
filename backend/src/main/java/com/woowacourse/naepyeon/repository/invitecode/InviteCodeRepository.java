b3a904158b5f82a94ac3878591674cb7b80338b6
yxxnghwan <younghwan960@gmail.com>
2022-09-25 15:43:15 +0900

    feat: 만료된 초대코드 삭제하는 벌크쿼리 구현

--- backend/src/main/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepository.java
@@ -1,10 +1,17 @@
 package com.woowacourse.naepyeon.repository.invitecode;
 
 import com.woowacourse.naepyeon.domain.invitecode.InviteCode;
+import java.time.LocalDateTime;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface InviteCodeRepository extends JpaRepository<InviteCode, Long> {
 
     Optional<InviteCode> findByCode(final String code);
+
+    @Query("delete from InviteCode ic where ic.expired < :now")
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    int deleteExpired(final LocalDateTime now);
 }
--- backend/src/test/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepositoryTest.java
@@ -1,11 +1,13 @@
 package com.woowacourse.naepyeon.repository.invitecode;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.woowacourse.naepyeon.config.JpaAuditingConfig;
 import com.woowacourse.naepyeon.config.QueryDslConfig;
 import com.woowacourse.naepyeon.domain.invitecode.InviteCode;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -35,5 +37,26 @@ class InviteCodeRepositoryTest {
 
         assertThatThrownBy(() -> inviteCodeRepository.save(inviteCode2))
                 .isInstanceOf(DataIntegrityViolationException.class);
+    }
+
+    @Test
+    @DisplayName("만료된 초대코드 데이터를 삭제한다.")
+    void deleteExpired() {
+        final InviteCode inviteCode1 = new InviteCode(null, "abc", 1L, LocalDateTime.now().minusHours(1));
+        final InviteCode inviteCode2 = new InviteCode(null, "def", 2L, LocalDateTime.now().minusHours(1));
+        final InviteCode inviteCode3 = new InviteCode(null, "ghi", 3L, LocalDateTime.now().plusHours(1));
+        final InviteCode inviteCode4 = new InviteCode(null, "jkl", 4L, LocalDateTime.now().plusHours(1));
+        inviteCodeRepository.save(inviteCode1);
+        inviteCodeRepository.save(inviteCode2);
+        inviteCodeRepository.save(inviteCode3);
+        inviteCodeRepository.save(inviteCode4);
+        em.flush();
+        em.clear();
+
+        inviteCodeRepository.deleteExpired(LocalDateTime.now());
+        final List<InviteCode> inviteCodes = inviteCodeRepository.findAll();
+
+        assertThat(inviteCodes).extracting("code")
+                .containsExactly(inviteCode3.getCode(), inviteCode4.getCode());
     }
 }
