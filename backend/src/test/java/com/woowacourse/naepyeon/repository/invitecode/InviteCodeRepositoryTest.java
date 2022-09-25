d4db907a3a10c31b64f4b945d750e84e725ead69
yxxnghwan <younghwan960@gmail.com>
2022-09-25 16:17:01 +0900

    style: 코드 리포맷팅

--- backend/src/test/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepositoryTest.java
+++ backend/src/test/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepositoryTest.java
@@ -1,6 +1,6 @@
 package com.woowacourse.naepyeon.repository.invitecode;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.woowacourse.naepyeon.config.JpaAuditingConfig;
@@ -8,7 +8,6 @@ import com.woowacourse.naepyeon.config.QueryDslConfig;
 import com.woowacourse.naepyeon.domain.invitecode.InviteCode;
 import java.time.LocalDateTime;
 import java.util.List;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
