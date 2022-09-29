29238cca638c6d8507e5cba5f6d31f4605a53ffd
yxxnghwan <younghwan960@gmail.com>
2022-09-29 17:12:13 +0900

    refactor: 쿼리실행 querydsl로 변경

--- backend/src/main/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepository.java
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepository.java
@@ -1,18 +1,7 @@
 package com.woowacourse.naepyeon.repository.invitecode;
 
 import com.woowacourse.naepyeon.domain.invitecode.InviteCode;
-import java.time.LocalDateTime;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-public interface InviteCodeRepository extends JpaRepository<InviteCode, Long> {
-
-    Optional<InviteCode> findByCode(final String code);
-
-    @Query("delete from InviteCode ic where ic.expired < :now")
-    @Modifying(clearAutomatically = true, flushAutomatically = true)
-    int deleteExpired(@Param("now") final LocalDateTime now);
+public interface InviteCodeRepository extends JpaRepository<InviteCode, Long>, InviteCodeRepositoryCustom {
 }
---
+++ backend/src/main/java/com/woowacourse/naepyeon/repository/invitecode/InviteCodeRepositoryImpl.java
@@ -0,0 +1,31 @@
+package com.woowacourse.naepyeon.repository.invitecode;
+
+import static com.woowacourse.naepyeon.domain.invitecode.QInviteCode.*;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.woowacourse.naepyeon.domain.invitecode.InviteCode;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class InviteCodeRepositoryImpl implements InviteCodeRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public long deleteExpired(final LocalDateTime now) {
+        return queryFactory.delete(inviteCode)
+                .where(inviteCode.expired.before(now))
+                .execute();
+    }
+
+    @Override
+    public Optional<InviteCode> findByCode(final String code) {
+        return Optional.ofNullable(
+                queryFactory.selectFrom(inviteCode)
+                .where(inviteCode.code.eq(code))
+                .fetchOne()
+        );
+    }
+}
