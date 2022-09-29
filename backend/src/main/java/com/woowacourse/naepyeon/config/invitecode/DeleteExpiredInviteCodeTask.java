5458fd41ec6fe0a330ad52291498c2e710c6bfbd
yxxnghwan <younghwan960@gmail.com>
2022-09-29 17:29:22 +0900

    fix: @Async @Scheduled 분리

--- backend/src/main/java/com/woowacourse/naepyeon/config/invitecode/DeleteExpiredInviteCodeTask.java
+++ backend/src/main/java/com/woowacourse/naepyeon/config/invitecode/DeleteExpiredInviteCodeTask.java
@@ -2,11 +2,9 @@ package com.woowacourse.naepyeon.config.invitecode;
 
 import com.woowacourse.naepyeon.repository.invitecode.InviteCodeRepository;
 import java.time.LocalDateTime;
-import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,9 +16,8 @@ public class DeleteExpiredInviteCodeTask {
     private final InviteCodeRepository inviteCodeRepository;
 
     @Async
-    @Scheduled(timeUnit = TimeUnit.HOURS, initialDelay = 0, fixedDelay = 12)
     @Transactional
-    public void scheduleDeleteExpiredInviteCode() {
+    public void deleteExpiredInviteCodes() {
         inviteCodeRepository.deleteExpired(LocalDateTime.now());
     }
 }
--- backend/src/main/java/com/woowacourse/naepyeon/config/invitecode/InviteCodeConfig.java
+++ backend/src/main/java/com/woowacourse/naepyeon/config/invitecode/InviteCodeConfig.java
@@ -1,10 +1,20 @@
 package com.woowacourse.naepyeon.config.invitecode;
 
+import java.util.concurrent.TimeUnit;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @EnableScheduling
 @Configuration
+@RequiredArgsConstructor
 public class InviteCodeConfig {
 
+    private final DeleteExpiredInviteCodeTask deleteExpiredInviteCodeTask;
+
+    @Scheduled(timeUnit = TimeUnit.HOURS, initialDelay = 0, fixedDelay = 12)
+    public void scheduleDeleteExpiredInviteCode() {
+        deleteExpiredInviteCodeTask.deleteExpiredInviteCodes();
+    }
 }
