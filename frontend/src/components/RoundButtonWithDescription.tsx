fe265c62087e5dfcedaa50f064482506b3fe6f08
prefer2 <sunho6200@gmail.com>
2022-10-01 16:17:20 +0900

    feat: RoundButtonWithDescription 구현

---
+++ frontend/src/components/RoundButtonWithDescription.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+type ButtonAttributes = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  description?: string;
+};
+
+const RoundButtonWithDescription = ({
+  type = "button",
+  onClick,
+  description,
+  children,
+}: ButtonAttributes) => {
+  return (
+    <StyledRoundButtonWithDescription>
+      <StyledRoundButton type={type} onClick={onClick}>
+        {children}
+      </StyledRoundButton>
+      <StyledDescription>{description}</StyledDescription>
+    </StyledRoundButtonWithDescription>
+  );
+};
+
+const StyledRoundButtonWithDescription = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  gap: 8px;
+
+  font-weight: 600;
+`;
+
+const StyledRoundButton = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 56px;
+  height: 56px;
+  background-color: ${({ theme }) => theme.colors.SKY_BLUE_200};
+  border-radius: 50%;
+
+  font-size: 28px;
+
+  &:hover {
+    color: ${({ theme }) => theme.colors.SKY_BLUE_300};
+  }
+`;
+
+const StyledDescription = styled.div`
+  text-align: center;
+  width: 70px;
+`;
+
+export default RoundButtonWithDescription;
