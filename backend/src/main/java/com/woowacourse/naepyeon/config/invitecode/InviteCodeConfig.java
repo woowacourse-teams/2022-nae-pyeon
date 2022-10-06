0e3d4c7e82253f7791b8867090e1c4c55c6bbe8e
yxxnghwan <younghwan960@gmail.com>
2022-10-07 01:18:38 +0900

    Revert "feat: shedlock 구현"
    This reverts commit 5789f0e04e9e3c539831614b2aa48b5cb19da0ce.

--- backend/src/main/java/com/woowacourse/naepyeon/config/invitecode/InviteCodeConfig.java
+++ backend/src/main/java/com/woowacourse/naepyeon/config/invitecode/InviteCodeConfig.java
@@ -3,7 +3,6 @@ package com.woowacourse.naepyeon.config.invitecode;
 import com.woowacourse.naepyeon.service.TeamService;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
-import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -16,7 +15,6 @@ public class InviteCodeConfig {
     private final TeamService teamService;
 
     @Scheduled(timeUnit = TimeUnit.HOURS, initialDelay = 0, fixedDelay = 12)
-    @SchedulerLock(name = "TaskScheduler_scheduledTask", lockAtLeastFor = "PT5M", lockAtMostFor = "PT14M")
     public void scheduleDeleteExpiredInviteCode() {
         teamService.deleteExpiredInviteCodes();
     }
--- backend/src/main/resources/schema.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE if not exists shedlock (
-                          name VARCHAR(64),
-                          lock_until TIMESTAMP(3) NULL,
-                          locked_at TIMESTAMP(3) NULL,
-                          locked_by VARCHAR(255),
-                          PRIMARY KEY (name)
-) engine=InnoDB default charset utf8mb4;
