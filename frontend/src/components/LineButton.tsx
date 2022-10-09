b68834fadc2c7286d5d85b1af7a8c512361d97a0
prefer2 <sunho6200@gmail.com>
2022-10-09 22:27:23 +0900

    refactor: LineButton prop type명 변경

--- frontend/src/components/LineButton.tsx
+++ frontend/src/components/LineButton.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface LineButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
 
 const LineButton = ({
   type = "button",
   onClick,
   disabled,
   children,
-}: ButtonProps) => {
+}: LineButtonProps) => {
   return (
     <StyledLineButton type={type} onClick={onClick} disabled={disabled}>
       {children}
