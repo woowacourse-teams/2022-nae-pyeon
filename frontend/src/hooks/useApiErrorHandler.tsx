286bdab579ffa02cc29bf91bfc066c4aa1ae51e9
soyi47 <soyi.dev@gmail.com>
2022-10-10 02:46:44 +0900

    fix: apiErrorHandler 내 타입 에러 수정

--- frontend/src/hooks/useApiErrorHandler.tsx
+++ frontend/src/hooks/useApiErrorHandler.tsx
@@ -5,46 +5,42 @@ import { useSnackbar } from "@/context/SnackbarContext";
 
 import { ApiErrorResponse } from "@/types/api";
 
+interface CustomError {
+  message: string;
+  errorCode: number;
+}
+
 const useApiErrorHandler = () => {
   const { openSnackbar } = useSnackbar();
   const navigate = useNavigate();
 
   const unauthorizedErrorHandler = () => {
-    navigate("logout");
+    navigate("/logout");
     openSnackbar(`로그인이 만료되었습니다. 다시 로그인 해주세요.`);
   };
 
-  const forbiddenErrorHandler = (error: AxiosError<ApiErrorResponse>) => {
-    const customErrorInfo = error.response!.data!;
-
-    switch (customErrorInfo.errorCode) {
-      case 4013: {
-        const requestUrl = error.config.url!;
-        const requestUrlPaths = requestUrl.split("/");
-        const teamId = requestUrlPaths.indexOf("teams") + 1;
-
-        navigate(`/team/${teamId}`);
-        openSnackbar(customErrorInfo.message);
-        break;
-      }
-      default:
-        openSnackbar(customErrorInfo.message);
-    }
+  const forbiddenErrorHandler = (customErrorInfo: CustomError) => {
+    navigate(`/`);
+    openSnackbar(customErrorInfo.message);
   };
 
-  const defaultErrorHandler = (errorMessage: ApiErrorResponse["message"]) => {
+  const defaultErrorHandler = (errorMessage: CustomError["message"]) => {
     openSnackbar(errorMessage);
   };
 
   const apiErrorHandler = (error: AxiosError<ApiErrorResponse>): void => {
-    const { status, data: customErrorInfo } = error.response!;
+    const { status, data } = error.response!;
+    const customErrorInfo = {
+      ...data!,
+      errorCode: Number(data!.errorCode),
+    };
 
     switch (status) {
       case 401: // Unauthorized
         unauthorizedErrorHandler();
         break;
       case 403: // Forbidden
-        forbiddenErrorHandler(error);
+        forbiddenErrorHandler(customErrorInfo);
         break;
       default:
         defaultErrorHandler(customErrorInfo.message);
