ff5f9c4507f4d87797b7dcd3d4d8a0b45ab9bff3
soyi47 <soyi.dev@gmail.com>
2022-10-10 12:06:08 +0900

    feat: useApiErrorHandler 에러 케이스 추가

--- frontend/src/hooks/useApiErrorHandler.tsx
+++ frontend/src/hooks/useApiErrorHandler.tsx
@@ -8,15 +8,61 @@ import { ApiErrorResponse } from "@/types/api";
 interface CustomError {
   message: string;
   errorCode: number;
+  requestUrl: string;
 }
 
 const useApiErrorHandler = () => {
   const { openSnackbar } = useSnackbar();
   const navigate = useNavigate();
 
-  const unauthorizedErrorHandler = () => {
-    navigate("/logout");
-    openSnackbar(`로그인이 만료되었습니다. 다시 로그인 해주세요.`);
+  const badRequestErrorHandler = (customErrorInfo: CustomError) => {
+    const { message, errorCode, requestUrl } = customErrorInfo;
+    const requestUrlPaths = requestUrl.split("/");
+    const teamIdIndex = requestUrlPaths.indexOf("teams") + 1;
+
+    switch (errorCode) {
+      // OAuth authorizationCode 또는 redirectUri가 잘못된 경우, 혹은 카카오의 Authorization Server가 정상작동하지 않는 경우
+      case 3014:
+        navigate("/login");
+        openSnackbar(message);
+        break;
+      // 이미 가입한 멤버가 모임에 재가입 시도
+      case 4007:
+      case 4008:
+        navigate(`/team/${teamIdIndex}`);
+        openSnackbar(message);
+        break;
+      // 초대코드 관련
+      case 4015:
+      case 4016:
+      case 4017:
+        navigate(`/team/${teamIdIndex}`);
+        openSnackbar(message);
+        break;
+      default:
+        openSnackbar(message);
+    }
+  };
+
+  const unauthorizedErrorHandler = (customErrorInfo: CustomError) => {
+    switch (customErrorInfo.errorCode) {
+      // 토큰 관련
+      case 3011:
+      case 3012:
+      case 3013:
+        navigate("/logout");
+        openSnackbar(customErrorInfo.message);
+        break;
+      // OAuth 로그인 실패
+      case 3015:
+      case 3016:
+      case 3017:
+        navigate("/login");
+        openSnackbar(customErrorInfo.message);
+        break;
+      default:
+        openSnackbar(customErrorInfo.message);
+    }
   };
 
   const forbiddenErrorHandler = (customErrorInfo: CustomError) => {
@@ -24,26 +70,29 @@ const useApiErrorHandler = () => {
     openSnackbar(customErrorInfo.message);
   };
 
-  const defaultErrorHandler = (errorMessage: CustomError["message"]) => {
-    openSnackbar(errorMessage);
-  };
-
   const apiErrorHandler = (error: AxiosError<ApiErrorResponse>): void => {
     const { status, data } = error.response!;
+    const requestUrl = error.config.url!;
+
     const customErrorInfo = {
       ...data!,
       errorCode: Number(data!.errorCode),
+      requestUrl,
     };
 
     switch (status) {
+      case 400: // Bad Request
+        badRequestErrorHandler(customErrorInfo);
+        break;
       case 401: // Unauthorized
-        unauthorizedErrorHandler();
+        unauthorizedErrorHandler(customErrorInfo);
         break;
       case 403: // Forbidden
         forbiddenErrorHandler(customErrorInfo);
         break;
+      case 404: // Not Found
       default:
-        defaultErrorHandler(customErrorInfo.message);
+        openSnackbar(customErrorInfo.message);
     }
 
     return;
