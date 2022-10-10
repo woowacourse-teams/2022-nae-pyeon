77e9863b96dfbf00b3a5188fa936d27bd852ef4a
soyi47 <soyi.dev@gmail.com>
2022-10-11 01:04:07 +0900

    feat: LogoutPage navigate 로직 추가

--- frontend/src/pages/LogoutPage/index.tsx
+++ frontend/src/pages/LogoutPage/index.tsx
@@ -1,13 +1,16 @@
 import { useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 import { UserContext } from "@/context/UserContext";
 import ErrorPage from "../ErrorPage";
 
 const LogoutPage = () => {
   const { logout } = useContext(UserContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     logout();
+    navigate("/", { replace: true });
   }, []);
 
   return <ErrorPage />;
