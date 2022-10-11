fc57c1cadec121fc09ff52efedf21a6b9887767b
Kim Seunglae <obey1342@gmail.com>
2022-10-11 16:38:19 +0900

    refactor: 자동으로 설정되는 값들 제거

--- backend/src/main/java/com/woowacourse/naepyeon/config/DataSourceConfig.java
+++ backend/src/main/java/com/woowacourse/naepyeon/config/DataSourceConfig.java
@@ -8,17 +8,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.DependsOn;
 import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.LazyConnectionDataSourceProxy;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableTransactionManagement
 @RequiredArgsConstructor
-@ComponentScan(basePackages = {"com.woowacourse.naepyeon"})
 public class DataSourceConfig {
 
     @Bean
