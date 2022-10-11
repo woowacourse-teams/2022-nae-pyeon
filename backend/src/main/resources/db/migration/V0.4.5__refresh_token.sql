a77bffbba70cfdcf17d4e05ce24bf9d3bbd39f99
yxxnghwan <younghwan960@gmail.com>
2022-10-12 00:58:58 +0900

    fix: 플라이웨이 sql문 추가

---
+++ backend/src/main/resources/db/migration/V0.4.5__refresh_token.sql
@@ -0,0 +1,12 @@
+CREATE TABLE refresh_token (
+                             refresh_token_id bigint not null auto_increment,
+                             value varchar(255) unique not null,
+                             member_id bigint not null,
+                             expired_time datetime not null,
+                             primary key (refresh_token_id)
+) engine=InnoDB default charset utf8mb4;
+
+alter table refresh_token
+    add constraint fk_refresh_token_member_id
+        foreign key (member_id)
+            references member (member_id) on delete cascade;
