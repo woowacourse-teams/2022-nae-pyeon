2c0bd63a4bbec01c0f8c79496e7344d24e6e7cad
soyi47 <soyi.dev@gmail.com>
2022-10-19 15:23:41 +0900

    fix: PolicyPage 상단 padding 추가

--- frontend/src/pages/PolicyPage/index.tsx
+++ frontend/src/pages/PolicyPage/index.tsx
@@ -4,7 +4,7 @@ import PageTitle from "@/components/PageTitle";
 
 const PolicyPage = () => {
   return (
-    <>
+    <StyledMain>
       <PageTitle title={"개인정보처리방침"} />
       <StyledContent>{`< 내편 >('https://www.naepyeon.site'이하 '내 편')은(는) 「개인정보 보호법」 제30조에 따라 정보주체의 개인정보를 보호하고 이와 관련한 고충을 신속하고 원활하게 처리할 수 있도록 하기 위하여 다음과 같이 개인정보 처리방침을 수립·공개합니다.
 
@@ -282,9 +282,13 @@ const PolicyPage = () => {
 예시 ) - 20XX. X. X ~ 20XX. X. X 적용 (클릭)
 
 예시 ) - 20XX. X. X ~ 20XX. X. X 적용 (클릭)`}</StyledContent>
-    </>
+    </StyledMain>
   );
 };
+
+const StyledMain = styled.main`
+  padding: 12px 0 0;
+`;
 
 const StyledContent = styled.div`
   white-space: pre-wrap;
