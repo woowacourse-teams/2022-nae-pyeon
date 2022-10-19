1dbad2f37fb257c24b5c6e6fbd4981498da905de
soyi47 <soyi.dev@gmail.com>
2022-10-19 22:06:24 +0900

    feat: NotificationPage EmptyState UI 추가

--- frontend/src/pages/NotificationPage/index.tsx
+++ frontend/src/pages/NotificationPage/index.tsx
@@ -1,5 +1,7 @@
 import styled from "@emotion/styled";
 
+import { useSnackbar } from "@/context/SnackbarContext";
+
 import PageTitle from "@/components/PageTitle";
 import NotificationItem from "@/pages/NotificationPage/components/NotificationItem";
 import useDeleteNotificationsAll from "./hooks/useDeleteNotificationAll";
@@ -7,13 +9,40 @@ import LineButton from "@/components/LineButton";
 import useReadNotifications from "@/hooks/useReadNotifications";
 import Loading from "@/components/Loading";
 
+import EmptyStateImg from "@/assets/images/empty-state.svg";
+
 const NotificationPage = () => {
-  const { data, isLoading } = useReadNotifications();
+  const { openSnackbar } = useSnackbar();
+
+  const { data, isLoading } = useReadNotifications({});
   const { mutate: deleteNotificationAll } = useDeleteNotificationsAll();
 
   if (isLoading) {
     return <Loading />;
   }
+
+  const NotificationListEmptyState = () => {
+    return (
+      <StyledEmpty>
+        <EmptyStateImg />
+        <StyledEmptyMessage>받은 알림이 없습니다.</StyledEmptyMessage>
+      </StyledEmpty>
+    );
+  };
+
+  const NotificationList = () => {
+    if (data?.notifications.length === 0) {
+      return <NotificationListEmptyState />;
+    }
+
+    return (
+      <StyledNotificationList>
+        {data?.notifications.map((notification) => (
+          <NotificationItem key={notification.id} notification={notification} />
+        ))}
+      </StyledNotificationList>
+    );
+  };
 
   return (
     <StyledPageContainer>
@@ -21,17 +50,17 @@ const NotificationPage = () => {
         <PageTitle title="알림 목록" titleAlign="left" />
         <LineButton
           onClick={() => {
+            if (data?.notifications.length === 0) {
+              openSnackbar("삭제할 알림이 없어요!");
+              return;
+            }
             deleteNotificationAll();
           }}
         >
           전체 알림 삭제
         </LineButton>
       </StyledTopSection>
-      <StyledNotificationList>
-        {data?.notifications.map((notification) => (
-          <NotificationItem key={notification.id} notification={notification} />
-        ))}
-      </StyledNotificationList>
+      <NotificationList />
     </StyledPageContainer>
   );
 };
@@ -40,6 +69,8 @@ const StyledPageContainer = styled.main`
   display: flex;
   flex-direction: column;
   gap: 20px;
+
+  height: calc(100vh - 100px);
 `;
 
 const StyledTopSection = styled.div`
@@ -59,6 +90,27 @@ const StyledNotificationList = styled.ul`
   display: flex;
   flex-direction: column;
   gap: 24px;
+`;
+
+const StyledEmpty = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  margin: 10vh 0;
+
+  svg {
+    font-size: 150px;
+  }
+`;
+
+const StyledEmptyMessage = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.colors.GRAY_400};
+
+  margin-bottom: 20px;
 `;
 
 export default NotificationPage;
